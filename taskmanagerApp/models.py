015ce59083c724f460d78dec24e8a274de021511
Ppllupold <kotnazar2005@gmail.com>
2024-10-22 18:41:49 +0300

    first migration with starting models configurations

--- taskmanagerApp/models.py
+++ taskmanagerApp/models.py
@@ -1,3 +1,71 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+PRIORITY_CHOICES = (
+    ("CRITICAL", "Critical"),
+    ("ESSENTIAL", "Essential"),
+    ("HIGH_PRIORITY", "High Priority"),
+    ("MEDIUM_PRIORITY", "Medium Priority"),
+    ("LOW_PRIORITY", "Low Priority"),
+)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    def str(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
+
+    def str(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def str(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=100, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="assigned_tasks")
+
+    def str(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    leader = models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True, related_name="led_teams")
+    members = models.ManyToManyField(Worker, related_name="teams")
+
+    def str(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    priority = models.CharField(max_length=100, choices=PRIORITY_CHOICES)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="projects")
+    tasks = models.ManyToManyField(Task, related_name="projects")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+
+    def str(self):
+        return self.name
