8a355ed2ef8d2668f33334a75a295874eed6bd09
Ppllupold <kotnazar2005@gmail.com>
2025-03-11 18:24:53 +0200

    fixed typo in team field

--- taskmanagerApp/models.py
+++ taskmanagerApp/models.py
@@ -63,9 +63,11 @@ class Project(models.Model):
     description = models.TextField()
     deadline = models.DateTimeField()
     priority = models.CharField(max_length=100, choices=PRIORITY_CHOICES)
-    team = models.ManyToManyField(Team, related_name="projects")
+    teams = models.ManyToManyField(Team, related_name="projects")
     created_at = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+
